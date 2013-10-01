181a272c1dfdde3e4cffd359518577cb84ca3861
Kevin Solorio <soloriok@gmail.com>
2013-10-01 13:34:27 -0500

    first pass without objects

--- comments.js
+++ comments.js
@@ -1,4 +1,31 @@
 // Shorthand for document ready
 $(function(){
-  // Your code goes here
+    $('#new_comment_button').click(function(event){
+        event.preventDefault();
+
+        var form = "<form id='new_comment' action='#' method='POST'>" +
+                        "<label>Author <input type='text' name='author'></label>" +
+                        "<label> Comment <textarea name='comment'></textarea></label>" +
+                        "<input type='submit'>" +
+                    "</form>"
+        $(this).hide();
+        $('#comment_area').append(form);
+    });
+
+    $('#comment_area').on('submit', '#new_comment', function(event){
+        event.preventDefault();
+
+        var author = $(this).find('input:first-child').val();
+        var comment = $(this).find('textarea').val();
+
+        var li = "<li>" +
+                    comment +
+                    "<span class='author'>" + author + "</span>" +
+                 "</li>";
+
+        $('#comment_list').append(li);
+        $(this).remove();
+        $('#new_comment_button').show();
+    });
 });
+
